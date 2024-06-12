e1ebe1be2b5770d8e53a2977da72f5b33db0df3a
Arnault DOUYERE <arnault.douyere@sii.fr>
2024-06-12 11:12:10 +0200

    fin cours

--- P3/P3C3/énoncé/main.py
+++ P3/P3C3/énoncé/main.py
@@ -1,5 +1,20 @@
-# Écrivez votre code ici !
+import csv
 
+def main():
+    with open("input.csv") as fichier_input:
+        reader = csv.DictReader(fichier_input, delimiter=',')
+        lignes = []
+        for ligne in reader:
+            lignes.append(ligne)
+    with open("output.csv", 'w', newline='') as fichier_output:
+        writer = csv.writer(fichier_output,delimiter=',')
+        writer.writerow(['nom', 'salaire'])
+        for ligne in lignes:
+            salaire = 0
+            salaire = int(ligne['heures_travaillees']) * 15
+            writer.writerow([ligne['nom'], salaire])
+           
+        
 
 # Ne touchez pas le code ci-dessous
 if __name__ == "__main__":
